71501c127148f68c0243237a63789247157b4fef
root <root@ip-172-31-89-158.ec2.internal>
2026-03-28 22:09:00 +0000

    add rds terraform file

--- terraform-ecs-fargate-fullstack-app/rds/rds.tf
+++ terraform-ecs-fargate-fullstack-app/rds/rds.tf
@@ -1,82 +1,26 @@
-
-
 provider "aws" {
   region = "us-east-1"
-
 }
-
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["ecs-vpc"]
-  }
-}
-
-data "aws_subnet" "private1" {
-  vpc_id            = data.aws_vpc.vpc.id
-  availability_zone = "us-east-1a"
-
-  filter {
-    name   = "tag:Name"
-    values = ["ecs-private1"]
-  }
-}
-
-data "aws_subnet" "private2" {
-  vpc_id            = data.aws_vpc.vpc.id
-  availability_zone = "us-east-1b"
-
-  filter {
-    name   = "tag:Name"
-    values = ["ecs-private2"]
-  }
-}
-
-data "aws_security_group" "sg" {
-  vpc_id = data.aws_vpc.vpc.id
-
-  filter {
-    name   = "tag:Name"
-    values = ["lb-sg"]
-  }
-}
-
-
 
 resource "aws_db_instance" "rds" {
-  allocated_storage       = 20
-  identifier              = "book-rds"
-  db_subnet_group_name    = aws_db_subnet_group.sub-grp.id
-  engine                  = "mysql"
-  engine_version          = "8.4.7"
-  instance_class          = "db.t3.micro"
-  multi_az                = true
-  db_name                 = "mydb"
-  username                = "admin"
-  password                = "Yaswanth123reddy"
-  skip_final_snapshot     = true
-  vpc_security_group_ids  = [data.aws_security_group.sg.id]
-  depends_on              = [aws_db_subnet_group.sub-grp]
-  publicly_accessible     = true # Make RDS publicly accessible
-  backup_retention_period = 7
-  apply_immediately       = true # ✅ this is the key
+  identifier = "book-rds"
 
+  engine         = "mysql"
+  engine_version = "8.0"
 
-  tags = {
-    DB_identifier = "book-rds"
-  }
-}
+  instance_class = "db.t3.micro"
 
-resource "aws_db_subnet_group" "sub-grp" {
-  name       = "rds"
-  subnet_ids = [data.aws_subnet.private1.id, data.aws_subnet.private2.id]
+  allocated_storage = 20
 
-  tags = {
-    Name = "My-DB-subnet-group"
-  }
-}
+  db_name  = "bookdb"
+  username = "admin"
+  password = "Admin1234!"
 
+  backup_retention_period = 1
+  multi_az               = false
 
-output "rds_address" {
-  value = aws_db_instance.rds.address
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = ["sg-0278aec1b35fc3645"]
 }
